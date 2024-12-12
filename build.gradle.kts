8833f13df07654d60163f8687d543b904f15935b
n0roo <n0roo@n0rooui-MacBookPro.local>
2024-12-13 00:31:55 +0900

    initial spring exam project

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    val kotlinVersion = "1.9.25"
+    val springBootVersion = "3.4.0"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+
+    id("org.jetbrains.kotlin.kapt") version kotlinVersion
+    id("org.springframework.boot") version springBootVersion
+
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+// Jpa Kotlin Plugin
+allOpen {
+    annotation("javax.persistence.Entity")
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "kotlin-kapt")
+    apply(plugin = "org.asciidoctor.jvm.convert")
+
+    group = "dev.n0roo.toy"
+    version = "0.0.1"
+
+    extra["snippetsDir"] = file("build/generated-snippets")
+    extra["springCloudVersion"] = "2024.0.0"
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        }
+    }
+
+
+    dependencies {
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+        // spring dev tools 및 configuration, Spring Test 모듈은 편의상 미리 주입.
+        implementation("org.springframework.boot:spring-boot-starter-logging")
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+        developmentOnly("org.springframework.boot:spring-boot-devtools")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.projectreactor:reactor-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+
+
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.test {
+        outputs.dir(project.extra["snippetsDir"]!!)
+    }
+
+    tasks.asciidoctor {
+        inputs.dir(project.extra["snippetsDir"]!!)
+        dependsOn(tasks.test)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "modern-spring-exam"
+
+include("components:common")  // 공통 Util, Const, Type, Model 등 Spring 이나 여타 의존성이 없는 컴포넌트
+include("components:mail-sender")   // SMTP email Sender
+
+include("domain:common-cached")
+include("domain:authenticate-entities") // authenticate 는 사용자의 인증만을 담당하므로, 인증만을 위한 entity 를 제어한다.
+include("domain:samples-entities")   // sample entity
+
+include("service:authenticate-api")  // 인증 서버
+include("service:sample-api")  // 인증 서버
+
+include("api-gateway")          // 게이트 웨이 서버
+include("api-discovery")        // eureka discovery register
